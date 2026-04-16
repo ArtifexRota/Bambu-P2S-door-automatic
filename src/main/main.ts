cb8d67cace08730e5ffb0bd32bf6a2bc030f6eae
unknown <nils.belling@web.de>
2026-04-16 19:42:45 +0200

    More Languages

--- src/main/main.ts
+++ src/main/main.ts
@@ -194,6 +194,7 @@ function createWindow(): void {
   mainWindow = new BrowserWindow({
     width: 1200,
     height: 900,
+    autoHideMenuBar: true,
     webPreferences: {
       // Nutzt den absoluten Pfad zur preload.js
       preload: preloadPath,
